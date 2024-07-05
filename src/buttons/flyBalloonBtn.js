897f654c740279d967c1709b5d92c9a93dac729b
jinrongda <jinrongda@meituan.com>
2024-07-05 23:02:26 +0800

    气球

--- src/buttons/flyBalloonBtn.js
+++ src/buttons/flyBalloonBtn.js
@@ -1,10 +1,12 @@
+const balloonBorder = document.querySelector('.balloon-border');
+
 const flyBalloonBtn = document.createElement('button');
 flyBalloonBtn.textContent = "Fly With Balloons";
 flyBalloonBtn.classList.add('style-button');
 flyBalloonBtn.classList.add('btn-position');
 flyBalloonBtn.classList.add('btn-appear_animation');
 flyBalloonBtn.addEventListener('click', () => {
-    console.log('flyBalloonBtn');
+    balloonBorder.classList.add('fly-end');
 }, 0)
 
 export default flyBalloonBtn;
